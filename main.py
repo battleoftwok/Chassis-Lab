ed1502b21062adbf3a9658648708788f77fd8d4d
Battle-Of-Two-K <tr.penrose@mail.ru>
2022-10-02 20:45:50 +0300

    Реализовал прорисовку графика

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from pprint import pprint
-
 import matplotlib.pyplot as plt
 from pathlib import Path
 from __log__ import log
 import yaml
-
 
 ENCODING = 'utf-8'
 UNIT = 10
@@ -61,51 +59,57 @@ def add_new_variant(var_num: int, variants_data: dict):
 
 
 def pneumatics_selection(pneumatics: dict, var_data: dict, amount_wheels: int):
-
     for item in pneumatics["pneumatics"]:
-        if (var_data['Pст.пос, [Н]'] / (UNIT * amount_wheels) < item["Pст.пос, [кгс]"]) & \
-                (var_data['Pст.взл, [Н]'] / (UNIT * amount_wheels) < item["Pст.взл, [кгс]"]) & \
-                (var_data['Vвзл, [км/ч]'] / amount_wheels < item["Vвзл, [км/ч]"]) & \
-                (var_data['Vпос, [км/ч]'] / amount_wheels < item["Vпос, [км/ч]"]):
+
+        if ((var_data['Pст.пос, [Н]'] / (UNIT * amount_wheels) < item["Pст.пос, [кгс]"]) &
+            (var_data['Pст.взл, [Н]'] / (UNIT * amount_wheels) < item["Pст.взл, [кгс]"]) &
+            (var_data['Vвзл, [км/ч]'] / amount_wheels < item["Vвзл, [км/ч]"]) &
+            (var_data['Vпос, [км/ч]'] / amount_wheels < item["Vпос, [км/ч]"])) & \
+                (str_into_tuple(item["p0, [кгс/см^2]"])[0] <= var_data["p0 * 10^5, [Па]"]
+                 <= str_into_tuple(item["p0, [кгс/см^2]"])[1]):
             return item
 
 
-def plot():
+def plot(data: dict):
     figure, axis = plt.subplots()
     figure.subplots_adjust(right=0.75)
 
-    twin1 = axis.twinx()
-    twin2 = axis.twinx()
+    crimping = axis.twinx()
+    force = axis.twinx()
 
-    # Offset the right spine of twin2.  The ticks and label have already been
-    # placed on the right by twinx above.
-    twin2.spines.right.set_position(("axes", 1.2))
+    force.spines.right.set_position(("axes", 1.2))
 
-    p1, = axis.plot([0, 1, 2], [0, 1, 2], "b-", label="Density")
-    p2, = twin1.plot([0, 1, 2], [0, 3, 2], "r-", label="Temperature")
-    p3, = twin2.plot([0, 1, 2], [50, 30, 15], "g-", label="Velocity")
+    pressure_vals = str_into_tuple(data["p0, [кгс/см^2]"])
+    work_vals = str_into_tuple(data["Aм.д, [кг*м]"])
+    crimping_vals = str_into_tuple(data["delta_м.д, [мм]"])
+    force_vals = str_into_tuple(data["Pм.д, [кгс]"])
 
-    axis.set_xlim(0, 2)
-    axis.set_ylim(0, 2)
-    twin1.set_ylim(0, 4)
-    twin2.set_ylim(1, 65)
+    p1, = axis.plot(pressure_vals, work_vals, "b-", label="Макс. доп. работа")
+    p2, = crimping.plot(pressure_vals, crimping_vals, "r-", label="Макс. до. обжатие")
+    p3, = force.plot(pressure_vals, force_vals, "g-", label="Макс. доп. сила")
 
-    axis.set_xlabel("Distance")
-    axis.set_ylabel("Density")
-    twin1.set_ylabel("Temperature")
-    twin2.set_ylabel("Velocity")
+    # axis.set_xlim(0, 20)
+    # axis.set_ylim(0, 2000)
+    # crimping.set_ylim(0, 106)
+    # force.set_ylim(0, 20000)
+
+    axis.set_xlabel("p0, [кгс/см^2]")
+    axis.set_ylabel("Aмд, [даН * мм]")
+    crimping.set_ylabel("δ_мд, [мм]")
+    force.set_ylabel("Pмд, [даН]")
 
     axis.yaxis.label.set_color(p1.get_color())
-    twin1.yaxis.label.set_color(p2.get_color())
-    twin2.yaxis.label.set_color(p3.get_color())
+    crimping.yaxis.label.set_color(p2.get_color())
+    force.yaxis.label.set_color(p3.get_color())
 
     tkw = dict(size=4, width=1.5)
     axis.tick_params(axis='y', colors=p1.get_color(), **tkw)
-    twin1.tick_params(axis='y', colors=p2.get_color(), **tkw)
-    twin2.tick_params(axis='y', colors=p3.get_color(), **tkw)
+    crimping.tick_params(axis='y', colors=p2.get_color(), **tkw)
+    force.tick_params(axis='y', colors=p3.get_color(), **tkw)
     axis.tick_params(axis='x', **tkw)
 
     axis.legend(handles=[p1, p2, p3])
+    axis.grid()
 
     plt.show()
 
@@ -119,12 +123,14 @@ def run(variant_number: int, amount_wheels: int):
         add_new_variant(variant_number, input_data)
     else:
         pprint(pneumatics_selection(pneumatics_data, var_data, amount_wheels))
+        plot(pneumatics_selection(pneumatics_data, var_data, amount_wheels))
 
 
 if __name__ == '__main__':
+    # Инициализация входных данных:
     input_data = read_yml_file(Path('variants_data.yml'))
     log.info('Input data successfully initialized.')
 
     pneumatics_data = read_yml_file((Path('pneumatics.yml')))
 
-    run(8, 4)
+    run(1, 2)
